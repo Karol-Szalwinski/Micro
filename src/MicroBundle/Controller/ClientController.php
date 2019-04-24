111def4ade8878e2db3804033bcf834b198d695c
Karol-Szalwinski <karol.szalwinski@gmail.com>
2019-04-24 21:44:58 +0200

    Navigation menu in twig template

--- src/MicroBundle/Controller/ClientController.php
+++ src/MicroBundle/Controller/ClientController.php
@@ -88,7 +88,7 @@ class ClientController extends Controller
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('client_edit', array('id' => $client->getId()));
+            return $this->redirectToRoute('client_index');
         }
 
         return $this->render('client/edit.html.twig', array(
